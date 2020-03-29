f9be760be9e7c29b5a91a77daf7c20b31399050f
Jirayut Nimsaeng <jirayut@opsta.co.th>
2020-03-29 16:25:00 +0700

    Fix GitLab example module source

--- examples/gitlab/main.tf
+++ examples/gitlab/main.tf
@@ -28,7 +28,7 @@ provider "aws" {
 module "gitlab" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
-  # source = "github.com/opsta/terraform-aws.git//modules/gitlab?ref=master"
+  # source = "github.com/opsta/terraform-aws.git//modules/instance?ref=master"
   source = "../../modules/instance"
 
   instance_name          = var.instance_name
