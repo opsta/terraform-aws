1bc5416b8c72feb43f7235f6cfb8de94ace72a8a
Jirayut Nimsaeng <jirayut@opsta.co.th>
2020-04-15 12:24:01 +0700

    Fix security group and add latest ami id for each OS

--- modules/instance/main.tf
+++ modules/instance/main.tf
@@ -68,7 +68,7 @@ module "key_pair" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_security_group" "security_group" {
-  name        = "aws-instance"
+  name        = var.instance_name
   description = "Security group for the ${var.instance_name} instance"
   vpc_id      = var.vpc_id != "" ? var.vpc_id : data.aws_subnet_ids.default.vpc_id
 
@@ -112,40 +112,35 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# data "aws_ami" "centos" {
-#   owners      = ["679593333241"]
-#   most_recent = true
+data "aws_ami" "centos" {
+  owners      = ["679593333241"]
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["CentOS Linux 7 x86_64 HVM EBS *"]
-#   }
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
 
-#   filter {
-#     name   = "architecture"
-#     values = ["x86_64"]
-#   }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 
-#   filter {
-#     name   = "root-device-type"
-#     values = ["ebs"]
-#   }
-# }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
 
-# data "aws_ami" "ecs" {
-#   most_recent = true
-#   owners      = ["591542846629"] # AWS
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
 
-#   filter {
-#     name   = "name"
-#     values = ["*amazon-ecs-optimized"]
-#   }
-
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-# }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # DEPLOY INSTANCE IN THE DEFAULT VPC AND SUBNETS
--- modules/instance/outputs.tf
+++ modules/instance/outputs.tf
@@ -22,3 +22,18 @@ output "ssh_username" {
   description = "Instance SSH Username"
   value       = var.ami_ssh_user
 }
+
+output "aws_ami_ubuntu" {
+  description = "AMI ID for Ubuntu 18.04"
+  value       = data.aws_ami.ubuntu.id
+}
+
+output "aws_ami_centos" {
+  description = "AMI ID for CentOS 7"
+  value       = data.aws_ami.centos.id
+}
+
+output "aws_ami_amazon_linux" {
+  description = "AMI ID for Amazon Linux"
+  value       = data.aws_ami.amazon_linux.id
+}
